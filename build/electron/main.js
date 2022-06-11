e9addcb10f309c1d099b31009e7c85d14bc602f5
Cesio137 <nathan.cm77@gmail.com>
2022-06-11 14:31:47 -0300

    added base template

---
+++ build/electron/main.js
@@ -0,0 +1,59 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
+const path = require("path");
+const electron_devtools_installer_1 = require("electron-devtools-installer");
+function createWindow() {
+    // Create the browser window.
+    const mainWindow = new electron_1.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+    if (electron_1.app.isPackaged) {
+        // 'build/index.html'
+        mainWindow.loadURL(`file://${__dirname}/../index.html`);
+    }
+    else {
+        mainWindow.loadURL('http://localhost:3000/');
+        mainWindow.webContents.openDevTools();
+        // Hot Reloading on 'node_modules/.bin/electronPath'
+        require('electron-reload')(__dirname, {
+            electron: path.join(__dirname, '..', '..', 'node_modules', '.bin', 'electron' + (process.platform === "win32" ? ".cmd" : "")),
+            forceHardReset: true,
+            hardResetMethod: 'exit'
+        });
+    }
+    // and load the index.html of the app.
+    //mainWindow.loadFile(path.join(__dirname, "../index.html"));
+    // Open the DevTools.
+    //mainWindow.webContents.openDevTools();
+}
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+electron_1.app.on("ready", () => {
+    createWindow();
+    (0, electron_devtools_installer_1.default)(electron_devtools_installer_1.REACT_DEVELOPER_TOOLS)
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log('An error occurred: ', err));
+    electron_1.app.on("activate", function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (electron_1.BrowserWindow.getAllWindows().length === 0)
+            createWindow();
+    });
+});
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+electron_1.app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        electron_1.app.quit();
+    }
+});
+// In this file you can include the rest of your app"s specific main process
+// code. You can also put them in separate files and require them here.
+//# sourceMappingURL=main.js.map
